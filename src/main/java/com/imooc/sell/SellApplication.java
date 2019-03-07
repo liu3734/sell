0434d965c2e6df68f3f35461e12f9b076f99cf92
yingliu <woshibihu@qq.com>
2019-03-07 23:45:27 +0800

    fix(SellApplication.java):修改作者

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -3,6 +3,9 @@ package com.imooc.sell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 刘颖
+ */
 @SpringBootApplication
 public class SellApplication {
 
