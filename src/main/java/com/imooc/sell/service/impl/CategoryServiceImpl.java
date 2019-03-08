12feb5648ffb28223da8f05ffe0e455d2bd347e8
yingliu <yingliu@travelsky.com>
2019-03-08 16:54:30 +0800

    Fix：修改日志格式，数据库端口，添加商品服务接口

--- src/main/java/com/imooc/sell/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/imooc/sell/service/impl/CategoryServiceImpl.java
@@ -4,9 +4,11 @@ import com.imooc.sell.dataobject.ProductCategory;
 import com.imooc.sell.repository.ProductCategoryRepository;
 import com.imooc.sell.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
