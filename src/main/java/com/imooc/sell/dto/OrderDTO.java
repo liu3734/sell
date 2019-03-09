39fcc54a60655d629230ea8804be9fa968dfce25
yingliu <woshibihu@qq.com>
2019-03-09 16:50:34 +0800

    Feature：买家端，订单功能实现

---
+++ src/main/java/com/imooc/sell/dto/OrderDTO.java
@@ -0,0 +1,66 @@
+package com.imooc.sell.dto;
+
+import com.imooc.sell.dataobject.OrderDetail;
+import com.imooc.sell.enums.OrderStatusEnum;
+import com.imooc.sell.enums.PayStatusEnum;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class OrderDTO {
+    /**
+     * 订单id.
+     */
+    private String orderId;
+
+    /**
+     * 买家名字.
+     */
+    private String buyerName;
+
+    /**
+     * 买家手机号.
+     */
+    private String buyerPhone;
+
+    /**
+     * 买家地址.
+     */
+    private String buyerAddress;
+
+    /**
+     * 买家微信Openid.
+     */
+    private String buyerOpenid;
+
+    /**
+     * 订单总金额.
+     */
+    private BigDecimal orderAmount;
+
+    /**
+     * 订单状态, 默认为0新下单.
+     */
+    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+
+    /**
+     * 支付状态, 默认为0未支付.
+     */
+    private Integer payStatus = PayStatusEnum.WAIT.getCode();
+
+    /**
+     * 创建时间.
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间.
+     */
+    private Date updateTime;
+
+    private List<OrderDetail> orderDetailList;
+
+}
